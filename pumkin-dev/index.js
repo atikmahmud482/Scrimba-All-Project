8db0262f08cbae09650f0b8ed2633cf15a640926
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-19 20:30:08 +0600

    remove the heighlignt class

--- pumkin-dev/index.js
+++ pumkin-dev/index.js
@@ -1,39 +1,43 @@
-import { catsData } from '/data.js'
+import { catsData } from "/data.js";
 
-const emotionRadios = document.getElementById('emotion-radios')
+const emotionRadios = document.getElementById("emotion-radios");
 
-emotionRadios.addEventListener('change', highlightCheckedOption)
+emotionRadios.addEventListener("change", highlightCheckedOption);
 
-function highlightCheckedOption(e){
-/*
+function highlightCheckedOption(e) {
+  /*
 Challenge:
 1. Create an array of all items that have 
    the "radio" class.
 2. Iterate over the array and remove the 
    "highlight" class from each one.
-*/ 
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+*/
+  const radios = document.getElementsByClassName("radio");
+
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
 }
 
-function getEmotionsArray(cats){
-    const emotionsArray = []    
-    for (let cat of cats){
-        for (let emotion of cat.emotionTags){
-            if (!emotionsArray.includes(emotion)){
-                emotionsArray.push(emotion)
-            }
-        }
+function getEmotionsArray(cats) {
+  const emotionsArray = [];
+  for (let cat of cats) {
+    for (let emotion of cat.emotionTags) {
+      if (!emotionsArray.includes(emotion)) {
+        emotionsArray.push(emotion);
+      }
     }
-    return emotionsArray
+  }
+  return emotionsArray;
 }
 
-
-function renderEmotionsRadios(cats){
-        
-    let radioItems = ``
-    const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions){
-        radioItems += `
+function renderEmotionsRadios(cats) {
+  let radioItems = ``;
+  const emotions = getEmotionsArray(cats);
+  for (let emotion of emotions) {
+    radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
@@ -42,13 +46,9 @@ function renderEmotionsRadios(cats){
             value="${emotion}"
             name="emotions"
             >
-        </div>`
-    }
-    emotionRadios.innerHTML = radioItems
+        </div>`;
+  }
+  emotionRadios.innerHTML = radioItems;
 }
 
-renderEmotionsRadios(catsData)
-
-
-
-
+renderEmotionsRadios(catsData);
