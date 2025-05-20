a480ceb6c7937c1424063fae1477439f1c553008
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-20 22:07:59 +0600

    Render the tweets to the feed

--- twiter-clone-dev/index.js
+++ twiter-clone-dev/index.js
@@ -7,12 +7,8 @@ tweetBtn.addEventListener("click", function () {
 });
 
 function getFeedHtml() {
-  /*
-Challenge:
-1. Replace the for of with a forEach.
-*/
-
   let feedHtml = ``;
+
   tweetsData.forEach(function (tweet) {
     feedHtml += `
 <div class="tweet">
@@ -40,4 +36,15 @@ Challenge:
   return feedHtml;
 }
 
-console.log(getFeedHtml());
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
+  /*
+Challenge:
+1. Take control of the ‘feed’ div.
+2. Render the HTML returned by the getFeedHtml 
+   function to the 'feed' div. 
+   See if you can do this with just one line of code!
+*/
+}
+
+render();
