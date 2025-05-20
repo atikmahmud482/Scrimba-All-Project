f5340b9f5d0f4182c8baa6a1219095c4c42e9abe
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-20 22:02:34 +0600

    use forEach to build the html

--- twiter-clone-dev/index.js
+++ twiter-clone-dev/index.js
@@ -7,9 +7,13 @@ tweetBtn.addEventListener("click", function () {
 });
 
 function getFeedHtml() {
-  let feedHtml = ``;
+  /*
+Challenge:
+1. Replace the for of with a forEach.
+*/
 
-  for (let tweet of tweetsData) {
+  let feedHtml = ``;
+  tweetsData.forEach(function (tweet) {
     feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
@@ -30,43 +34,10 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
-</div>`;
-  }
-  console.log(feedHtml);
-  /*
-Challenge:
-1. Use a "for of" to iterate over the data and 
-   create HTML string for each tweet using the 
-   boilerplate below. Replace UPPERCASE text
-   with data from the tweets. 
-2. Store this HTML in a let called "feedHtml".
-3. Log out feedHtml.
-4. Call getFeedHtml to check it's working.
-*/
+</div>
+`;
+  });
+  return feedHtml;
 }
 
-getFeedHtml();
-
-/*
-<div class="tweet">
-    <div class="tweet-inner">
-        <img src="URL OF PROFILE PIC" class="profile-pic">
-        <div>
-            <p class="handle">TWEET HANDLE</p>
-            <p class="tweet-text">TWEET TEXT</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                    NUMBER OF REPLIES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF LIKES
-                </span>
-                <span class="tweet-detail">
-                    NUMBER OF RETWEETS
-                </span>
-            </div>   
-        </div>            
-    </div>
-</div>
-
-*/
+console.log(getFeedHtml());
