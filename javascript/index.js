f937223df0354b08403254edb767f93f6e9eb3c4
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-22 21:25:41 +0600

    Beyond Function Declarations 3: Arrow Functions

--- javascript/index.js
+++ javascript/index.js
@@ -69,12 +69,14 @@ Challenge:
     1. Convert this function declaration to a function expression.
 */
 
-const getTheftAlert = function (numberOfTransactionsHour) {
-  if (numberOfTransactionsHour > 5) {
-    return `You have made ${numberOfTransactionsHour} transactions 
-                in the past hour. We think your card might have been 
-                compromised`;
-  }
-};
+// const getTheftAlert = function (numberOfTransactionsHour) {
+//   if (numberOfTransactionsHour > 5) {
+//     return `You have made ${numberOfTransactionsHour} transactions
+//                 in the past hour. We think your card might have been
+//                 compromised`;
+//   }
+// };
 
-console.log(getTheftAlert(6));
+// console.log(getTheftAlert(6));
+
+/* Beyond Function Declarations 3: Arrow Functions */
