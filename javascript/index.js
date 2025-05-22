073bb7839ceae087f866af871013db4901a9fc0e
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-22 22:01:06 +0600

    Aside .reduce() challenge

--- javascript/index.js
+++ javascript/index.js
@@ -105,14 +105,28 @@ Challenge:
 //    driver's actual speed.
 
 /* array.reduce() */
-const rainJanuaryByWeek = [10, 20, 0, 122];
+// const rainJanuaryByWeek = [10, 20, 0, 122];
 
-const totalRainfallJanuary = rainJanuaryByWeek.reduce(function (
-  total,
-  currentElement
-) {
-  console.log("total: " + total, "currentElement: " + currentElement);
-  return total + currentElement;
+// const totalRainfallJanuary = rainJanuaryByWeek.reduce(function (
+//   total,
+//   currentElement
+// ) {
+//   console.log("total: " + total, "currentElement: " + currentElement);
+//   return total + currentElement;
+// });
+
+// console.log(totalRainfallJanuary);
+
+/* Aside .reduce() challenge */
+const grades = [75, 83, 66, 43, 55, 99, 87, 16, 89, 64, 70, 80, 94, 77, 66, 73];
+
+const totalGrades = grades.reduce(function (total, currentGrade) {
+  return total + currentGrade;
 });
 
-console.log(totalRainfallJanuary);
+console.log(`The class average is ${totalGrades / grades.length}`);
+/*
+Challenge
+1. Use the .reduce() method to find the total of all of the students grades.
+2. Do some simple maths to log out the class average. 
+*/
