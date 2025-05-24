1eb60bcfdf1d8ad6eb09cdf9a2e15a3f7e70845c
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-24 19:27:31 +0600

    array(map,filter,reduce)

--- javascript/interview-practice.js
+++ javascript/interview-practice.js
@@ -2,10 +2,19 @@
 function greeting(name) {
   return `Hello, ${name}`;
 }
-greeting("atik");
+// console.log(greeting("atik"));
 
 // arow function
 const greeting2 = (name) => {
   return `Hello, ${name}`;
 };
-greeting2("atik");
+// console.log(greeting2("atik"));
+
+/* Array */
+const nums = [1, 2, 3, 4, 5];
+const doubled = nums.map((n) => n * 2);
+const even = nums.filter((n) => n % 2 === 0);
+const sum = nums.reduce((acc, n) => acc + n, 0);
+// console.log(doubled);
+// console.log(even);
+// console.log(sum);
