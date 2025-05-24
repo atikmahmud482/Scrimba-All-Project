8ed4d2de7d73db0e1bfc8d08d314cd4ae10b4cdd
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-24 19:13:11 +0600

    function and arow function

---
+++ javascript/interview-practice.js
@@ -0,0 +1,11 @@
+// function
+function greeting(name) {
+  return `Hello, ${name}`;
+}
+greeting("atik");
+
+// arow function
+const greeting2 = (name) => {
+  return `Hello, ${name}`;
+};
+greeting2("atik");
