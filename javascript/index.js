50749b4e867af2607b92d5a77e8b5bd9eae86b7b
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-25 23:10:53 +0600

    Debugging: Errors

--- javascript/index.js
+++ javascript/index.js
@@ -386,22 +386,55 @@ Challenge:
 Rebuild this constructor function as a class.
 */
 
-class Character {
-  constructor(name) {
-    this.name = name;
-    this.collectedItemsArr = [];
-  }
+// class Character {
+//   constructor(name) {
+//     this.name = name;
+//     this.collectedItemsArr = [];
+//   }
 
-  addItem(item) {
-    this.collectedItemsArr.push(item);
-    console.log(`${this.name} now has: ${this.collectedItemsArr.join(", ")}`);
+//   addItem(item) {
+//     this.collectedItemsArr.push(item);
+//     console.log(`${this.name} now has: ${this.collectedItemsArr.join(", ")}`);
+//   }
+// }
+
+// const wizard = new Character("Merlin");
+// const witch = new Character("Hermione");
+// wizard.addItem("a wand");
+// wizard.addItem("a map");
+// wizard.addItem("a rope");
+// witch.addItem("a map");
+// witch.addItem("a rope");
+
+/* Debugging: Errors */
+const ageInput = document.getElementById("age");
+const resultDisplay = document.getElementById("result-display");
+const checkButton = document.getElementById("btn-check");
+
+//set minimum drinking and driving ages for a jurisdiction
+const minDrinkAge = 21;
+const minDriveAge = 16;
+
+//check button click event listener
+checkButton.addEventListener("click", function () {
+  let message = "";
+  const age = ageInput.value;
+  // Age is below the minDrinkAge and minDriveAge
+  if (age > minDrinkAge && age > minDriveAge) {
+    message = `I'm sorry, you cannot drink or drive ⛔`;
+    // Relevant to countries where minDrinkAge is less than minDriveAge
+  } else if (age >= minDrinkAge && age < minDriveAge) {
+    message = "You can drink 🍺 but you cannot drive";
+    // Relevant to countries where minDriveAge is less than minDrinkAge
+  } else if (age >= minDriveAge && age < minDrinkAge) {
+    message = "You can drive 🚗 but you cannot drink";
+    // Age is above the minDrinkAge and minDriveAge
+  } else {
+    message = "You can drink 🍺 and drive 🚗 (not at the same time though!)";
   }
+  renderMessage(message);
+});
+
+function renderMessage(message) {
+  resultDisplay.innerText = message;
 }
-
-const wizard = new Character("Merlin");
-const witch = new Character("Hermione");
-wizard.addItem("a wand");
-wizard.addItem("a map");
-wizard.addItem("a rope");
-witch.addItem("a map");
-witch.addItem("a rope");
