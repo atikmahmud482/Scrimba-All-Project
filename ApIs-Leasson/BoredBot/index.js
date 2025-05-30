a3336c2b498ad0aaad54fb221f5386b2943c4899
atikmahmud482 <atikmahmud482@gmail.com>
2025-05-30 23:58:20 +0600

    boredBot javascript

--- ApIs-Leasson/BoredBot/index.js
+++ ApIs-Leasson/BoredBot/index.js
@@ -1,18 +1,16 @@
 /**
 Challenge: 
 
-- Start building out the BoredBot Skeleton however you'd like. 
-    That will include:
-    - A title for the app ("BoredBot" might be a good start 😉)
-    - A placeholder element that will be populated with the random 
-      idea we get from the API
-    - A button to click for triggering the GET request to the Bored API. 
-      (Don't worry about implementing the button quite yet)
-*/
+When the button is clicked, call out to the Bored API
+(URL: https://apis.scrimba.com/bored/api/activity)
+and replace the h4 with the activity from the API
 
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
+*/
+document.getElementById("get-activity").addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.getElementById("activity").textContent = data.activity;
+    });
+});
