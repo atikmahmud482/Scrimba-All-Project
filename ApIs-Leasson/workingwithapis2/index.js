f60c568044bb91750a3db282b07fc1ce832f3999
atikmahmud482 <atikmahmud482@gmail.com>
2025-06-16 21:19:02 +0600

    Crypto - Display name and icon

--- ApIs-Leasson/workingwithapis2/index.js
+++ ApIs-Leasson/workingwithapis2/index.js
@@ -1,24 +1,35 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
-
-/**
- * Challenge: Pull down the cryptocurrency data for dogecoin from the 
- * CoinGecko API and log it to the console
- * 
- * Also add a .catch() method to console any errors that might occur to the console
- */
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    /**
+     * Challenge: Add the name and icon of the cryptocurrency
+     * to the upper-left of the dashboard page
+     *
+     * Use `data.name` and `data.image.small` to access that info
+     */
+    document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `;
+  })
+  .catch((err) => console.error(err));
